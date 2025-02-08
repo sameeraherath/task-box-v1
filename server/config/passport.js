905fe778038c7a1edb5fc73c70efd6bfa9a2d2d4
sameeraherath <sameerastar6@gmail.com>
2025-02-08 09:21:59 +0530

    Add missing file

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 // Configure Google OAuth strategy for user authentication
 
