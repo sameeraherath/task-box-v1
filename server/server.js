d22276f31631d6c2ebd2de20d42fb59088a842dc
sameeraherath <sameerastar6@gmail.com>
2025-02-09 08:47:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,8 +10,13 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 5002;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
