d5b7d0e7f5d76d0d778d36ce408d209609a48588
sameeraherath <sameerastar6@gmail.com>
2025-02-12 12:07:32 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5002/google/callback",
+      callbackURL: "https://task-box-v1-server.onrender.com/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
